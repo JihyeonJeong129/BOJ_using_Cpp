a73d0ab675ad1d9dc106ac9273714bb5e007fe65
JiHyeonJeong <jh.jeong129@gmail.com>
2023-05-13 13:41:24 +0900

    10869 complete!

---
+++ 10869/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    
+    int num1,num2;
+    
+    std::cin >> num1 >> num2;
+
+    std::cout << num1+num2 << std::endl;
+    std::cout << num1-num2 << std::endl;
+    std::cout << num1*num2 << std::endl;
+    std::cout << num1/num2 << std::endl;
+    std::cout << num1%num2 << std::endl;
+    
+    return 0;
+}
+
