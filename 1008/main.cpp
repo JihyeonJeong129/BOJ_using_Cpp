1663282d047b18d30c3d93790eb639c90211c995
JiHyeonJeong <jh.jeong129@gmail.com>
2023-05-13 13:53:45 +0900

    add 1008 lesson learn comment!

--- 1008/main.cpp
+++ 1008/main.cpp
@@ -6,7 +6,8 @@ int main(){
 
     std::cin >> num1 >> num2;
     
-    std::cout.precision(20);
+    std::cout.precision(20); 
+    // std::cout.precision(a) -> It expresses a significant number as many as a number.
     std::cout << (double)num1 / (double)num2;
 
     return 0;
