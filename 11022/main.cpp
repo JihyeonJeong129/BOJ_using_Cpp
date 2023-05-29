75fed87d4dee218970575b8a6583f22a81fa5a62
JiHyeonJeong <jh.jeong129@gmail.com>
2023-05-29 17:51:50 +0900

    11022 complete!!

---
+++ 11022/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int count;
+
+    int num1, num2;
+
+    std::cin >> count;
+
+    for(int i = 1; i <= count; i++){
+        std::cin >> num1 >> num2;
+        std::cout << "Case #" << i << ": " << num1 
+        << " + " << num2 << " = " << num1 + num2 << "\n";
+    }
+
+    return 0;
+}
