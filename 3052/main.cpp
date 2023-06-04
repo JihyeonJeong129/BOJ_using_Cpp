9110bb78889263b4b8cbc61cf80ae6f834d31077
JiHyeonJeong <jh.jeong129@gmail.com>
2023-06-04 14:50:23 +0900

    3052 init

---
+++ 3052/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+    int mod_arr[10] = {};
+
+    int count = 0;
+    
+    int input_num;
+
+
+    std::cin >> input_num;
+
+    mod_arr[0] = (input_num % 42);
+
+    count = count + 1;
+
+
+    for(int i = 1; i < 10; i++){
+        
+        std::cin >> input_num;
+
+        for(int j = 0; j < count; j++){
+            
+            if((input_num % 42) == mod_arr[j]){
+                continue;
+            }
+        }
+
+        mod_arr[count++] = (input_num % 42); 
+
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
+}
