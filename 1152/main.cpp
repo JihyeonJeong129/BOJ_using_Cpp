764a18254cd57ddfc9a3a94ae461c7c5e6fe5ff2
JiHyeonJeong <jh.jeong129@gmail.com>
2023-06-11 18:00:31 +0900

    1152 complete.

--- 1152/main.cpp
+++ 1152/main.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 int main()
 {
     std::string str;
 
     getline(std::cin, str);
+    
+    
+    char *cp_str = new char[str.size()];
+
+    strcpy(cp_str, str.c_str());
+
+   //if string is empty
+    if(strcmp(cp_str, " ") == 0){
+        std::cout << 0;
+        return 0;
+    }
+
 
     int count = 1;
 
-    for(int i = 0; i < str.size(); i++){
-        
-        if(str[i] == ' '){
-            count = count + 1;
-        }
+    char *ptr = strtok(cp_str, " "); 
+    
+    while(strtok(NULL, " ")){
+        count = count + 1;
     }
 
     std::cout << count;
+
+    delete cp_str;
 
     return 0;
 }
