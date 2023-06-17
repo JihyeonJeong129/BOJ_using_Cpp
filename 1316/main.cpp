af280533dc94d31c71023ceac8fa165d8c0dca26
JiHyeonJeong <jh.jeong129@gmail.com>
2023-06-17 23:04:34 +0900

    1316 init commit

---
+++ 1316/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int count;
+
+    std::cin >> count;
+
+    for(int i = 0; i < count; i++){
+        
+        std::string str;
+        std::cin >> str;
+        
+        for(j = 0; j < str.size(); j++){
+            
+
+        }
+    }
+
+    return 0;
+}
