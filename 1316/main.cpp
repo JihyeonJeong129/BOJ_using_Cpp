7f45c588630abcf6a8515fc1f384a0202730cf31
JiHyeonJeong <jh.jeong129@gmail.com>
2023-06-30 22:32:36 +0900

    1316 completegit add .

--- 1316/main.cpp
+++ 1316/main.cpp
@@ -7,16 +7,56 @@ int main()
 
     std::cin >> count;
 
+    int world_count = 0;
+
     for(int i = 0; i < count; i++){
         
         std::string str;
         std::cin >> str;
+
+        int flag_arr[26] = {0, };
+
         
-        for(j = 0; j < str.size(); j++){
-            
+        
+        for(int j = 0; j < str.size();){
+
+            char comp = str[j];
+
+            flag_arr[str[j] - 'a'] = 1;
+
+            j = j + 1;
+
+            while(true){
+                
+                if(comp != str[j]){
+                    
+                    break;
+                }
+
+                if(j >= str.size()){
+                    
+                    break;
+                }
+
+                j++;
+
+            }
+
+            if(flag_arr[str[j] - 'a'] == 1){
+                
+                break;
+            }
+
+
+            if((j == (str.size() - 1) && str[j] == comp) || j == (str.size())){
+                world_count++;
+                break;
+            }
 
         }
     }
+
+    std::cout << world_count << std::endl;
 
     return 0;
 }
