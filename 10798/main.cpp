3c7c22e715c5fefe899f9aeb9c1bf57ce5db57cb
JiHyeonJeong <jh.jeong129@gmail.com>
2023-07-01 15:19:24 +0900

    10798 complete!

--- 10798/main.cpp
+++ 10798/main.cpp
@@ -5,34 +5,17 @@ int main()
 {
     std::string str[5];
 
-    int length[5] = {0, };
-
     for(int i = 0; i < 5; i++){
         
         std::cin >> str[i];
-        length[i] = str[i].size();
     }
         
-    int max_val;
-
-    max_val = length[0];
-
-    for(int i = 1; i < 5; i++){
-        
-        if(max_val < length[i]){
-            max_val = length[i];
-        }
-    }
-
-    for(int i = 0; i < max_val; i ++){
+    for(int i = 0; i < 15; i ++){
         
         for(int j = 0; j < 5; j++){
             
-            if(i > length[j]){
-                continue;
-            }
-
-            else{
+            if(i < str[j].size()){
+                
                 std::cout << str[j][i];
             }
         }
