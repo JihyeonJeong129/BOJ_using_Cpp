70c40e995b36442f7c8af314248b75f1820162d0
JiHyeonJeong <jh.jeong129@gmail.com>
2023-07-01 15:49:22 +0900

    2563 init

---
+++ 2563/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main()
+{
+    int count;
+
+    int area_count;
+
+    int paper[101][101];
+
+    std::cin >> count;
+
+    //array_reset
+
+    for(int i = 0; i < 101; i++){
+
+        for(int j = 0; j < 101; j++){
+                
+            paper[i][j] = 0;
+        }
+    }
+
+    std::cout << area_count;
+
+    for(int i = 0; i < count; i++){
+        
+        int num1, num2;
+
+        std::cin >> num1 >> num2;
+
+        for(int j = 0; j < 10; j++){
+            
+            for(int k = 0; k < 10; k++){
+                
+                paper[num1 + j][num2 + k]++;
+            }
+        }
+    }
+
+    for(int i = 0; i < 101; i++){
+
+        for(int j = 0; j < 101; j++){
+            
+            if(paper[i][j] != 0){
+                area_count++;
+            }
+        }
+    }
+
+    std::cout << area_count;
+
+    return 0;
+}
