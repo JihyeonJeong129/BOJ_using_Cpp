c84b908000586029c6a1241020077e97c2f7e2e5
JiHyeonJeong <jh.jeong129@gmail.com>
2023-07-21 18:20:50 +0900

    2903 complete!

---
+++ 2903/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+
+    int num;
+
+    std::cin >> num;
+
+    std::cout << (int)pow(pow(2, num)+1, 2) << std::endl;
+
+    return 0;
+
+}
---
+++ 9506/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int num;
+
+    return 0;
+}
