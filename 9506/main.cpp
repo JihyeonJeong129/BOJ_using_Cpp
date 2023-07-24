087a09d5ce6c7743811c70434206cb5182bb54e6
JiHyeonJeong <jh.jeong129@gmail.com>
2023-07-24 15:58:56 +0900

    9506 done!

--- 9506/main.cpp
+++ 9506/main.cpp
@@ -2,7 +2,59 @@
 
 int main()
 {
-    int num;
+
+    while(1){
+
+        int num;
+
+        std::cin >> num;
+
+        if(num == -1){
+            break;
+        }
+
+        int *arr = new int[10000];
+
+        int idx = 0;
+        
+        for(int i = 1; i < num; i ++){
+            
+            if(num % i == 0){
+                
+                arr[idx] = i;
+
+                idx = idx + 1;
+            }
+        }
+
+        int sum = 0;
+
+        
+        for(int i = 0; i < idx; i ++){
+           
+           sum = sum + arr[i];
+        }
+
+        if(sum == num){
+            
+            std::cout << num << " = ";
+
+            for(int i = 0; i < idx - 1; i ++){
+
+                std::cout << arr[i] << " + ";
+            }
+
+            std::cout << arr[idx - 1] << std::endl;
+        }
+
+        else{
+
+            std::cout << num << " is NOT perfect." << std::endl;
+        }
+
+    }
+
+
 
     return 0;
 }
