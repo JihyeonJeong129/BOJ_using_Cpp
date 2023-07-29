5f584825c38c4bcfba0e1d07b9071791ab860983
JiHyeonJeong <jh.jeong129@gmail.com>
2023-07-29 14:13:07 +0900

    24266 done!

---
+++ 24266/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    long num;
+
+    std::cin >> num;
+
+    std::cout << num * num * num << std::endl;
+
+    std::cout << "3" << std::endl;
+
+    return 0;
+}
