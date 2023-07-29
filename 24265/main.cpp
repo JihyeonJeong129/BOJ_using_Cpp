f49304cc4d24f3eb6e94ce68fff8cb5590e22306
JiHyeonJeong <jh.jeong129@gmail.com>
2023-07-29 14:11:22 +0900

    24265 done!

---
+++ 24265/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    long num;
+
+    std::cin >> num;
+
+    std::cout << num * (num-1) / 2 << std::endl;
+
+    std::cout << "2" << std::endl;
+
+    return 0;
+}
