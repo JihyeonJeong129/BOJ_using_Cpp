3de0efa5ace97a4a6d949bcd39842790eca36fe5
JiHyeonJeong <jh.jeong129@gmail.com>
2023-08-04 07:45:26 +0900

    24267 done!

---
+++ 24267/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    long long n;
+
+    std::cin >> n;
+
+    std::cout << (n)*(n-1)*(n-2)/6 << std::endl;
+
+    std::cout << "3" << std::endl;
+
+    return 0;
+}
