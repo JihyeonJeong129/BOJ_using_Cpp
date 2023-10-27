36acbb0b9f0f7b1d88af803f7e6c0caf4e9c3a13
JiHyeonJeong <jh.jeong129@gmail.com>
2023-10-27 23:12:18 +0900

    10989 first_ver

--- 10989/main.cpp
+++ 10989/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <cstring>
 
 int main()
 {
-    
+    long n;
+
+    std::cin >> n;
+
+    int *arr = new int[n];
+    int *count = new int[n];
+
+    memset(count, 0, sizeof(int) * n);
+
+    for(int i = 0; i < n; i++){
+        
+        std::cin >> arr[i];
+
+        count[arr[i]]++;
+
+    }
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < count[i]; j++){
+            
+            std::cout << i << "\n";
+        }        
+    }
 
     return 0;
 }
