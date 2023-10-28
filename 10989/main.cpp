c639c12fb1df3a47eb3190e36d8edc09069e3e24
JiHyeonJeong <jh.jeong129@gmail.com>
2023-10-28 19:56:25 +0900

    10989 done!

--- 10989/main.cpp
+++ 10989/main.cpp
@@ -3,24 +3,26 @@
 
 int main()
 {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(NULL);
+
     long n;
 
     std::cin >> n;
 
-    int *arr = new int[n];
-    int *count = new int[n];
-
-    memset(count, 0, sizeof(int) * n);
+    int count[10001] = {0, };
 
     for(int i = 0; i < n; i++){
         
-        std::cin >> arr[i];
+        int num;
 
-        count[arr[i]]++;
+        std::cin >> num;
 
+        count[num]++;
     }
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i <= 10000; i++){
 
         for(int j = 0; j < count[i]; j++){
             
