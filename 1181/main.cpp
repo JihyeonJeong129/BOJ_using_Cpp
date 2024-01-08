7d022d4b8a9d241aa164def29c2bcef87ebc3470
JiHyeonJeong <jh.jeong129@gmail.com>
2024-01-09 00:33:26 +0900

    1181 done!

--- 1181/main.cpp
+++ 1181/main.cpp
@@ -2,10 +2,14 @@
 #include <string>
 #include <algorithm>
 
-bool compare_length(std::string str1, std::string str2){
+bool compare(std::string str1, std::string str2){
 
 	if(str1.length() > str2.length()){
 		return false;
+	}
+	
+	else if(str1.length() == str2.length()){
+		return str1 < str2;
 	}
 
 	else{
@@ -13,24 +17,33 @@ bool compare_length(std::string str1, std::string str2){
 	}
 }
 
+
 int main()
 {
+	std::ios_base :: sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
 	int num;
 
 	std::cin >> num;
 
-	std::string *str = new std::string[num];
+	std::string *str = new std::string[num+1];
 
 	for(int i= 0; i < num; i++){
 		
 		std::cin >> str[i];
 	}
 
-	std::sort(str, str+num, compare_length);
+	std::sort(str, str+num, compare);
 
-	for(int i = 0; i < num; i++){
+	auto last = std::unique(str, str+num);
+
+	std::sort(str, last, compare);
+
+	for(int i = 0; &str[i] < last; i++){
 		
-		std::cout << str[i] << "\n";
+		std::cout << str[i] << "\n";	
 	}
 
 	
