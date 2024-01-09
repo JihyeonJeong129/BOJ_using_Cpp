93cd721c459d39a02742a96ec7bfc0a3d111177e
JiHyeonJeong <jh.jeong129@gmail.com>
2024-01-09 19:05:28 +0900

    10814 done!

--- 10814/main.cpp
+++ 10814/main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
+#include <algorithm>
+
+struct person
+{
+	int age;
+	std::string name;
+};
+
+bool compare(person p1, person p2){
+
+	if(p1.age < p2.age){
+		return true;
+	}
+
+	return false;
+}
 
 int main()
 {
 	int num;
 
 	std::cin >> num;
-
-	int *age = new int[num];
-
-	std::string *str = new std::string[num];
-
-	for(int i = 0 ; i < num; i++){
 	
-		std::cin >> num[i] >> str[i];
+	//memory allocation
+	struct person *data = new person[num];
+	
+	//data input
+	for(int i = 0; i < num; i++){
+	
+		std::cin >> data[i].age >> data[i].name;
 	}
 
+	std::stable_sort(data, data+num, compare);
 
+	//data print
+	for(int i = 0; i < num; i++){
+		
+		std::cout << data[i].age << " " << data[i].name << "\n";
+	}
+
+	//memory deallocation
+	delete []data;
 
 	return 0;
 }
