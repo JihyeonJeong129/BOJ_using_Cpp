0626aadcf63996a35513d065d8d19eb566f9cec0
JiHyeonJeong <jh.jeong129@gmail.com>
2024-01-12 18:40:16 +0900

    14425 done!

--- 14425/main.cpp
+++ 14425/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 int main()
 {
+	std::ios_base :: sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::map<std::string, int> data;
+
+	int m,n;
+
+	std::cin >> m >> n;
+
+//data insertion
+	for(int i = 0; i < m; i++){
+		
+		std::string input;
+		std::cin >> input;
+		data.insert({input, i});
+	}
+
+	int count = 0;
+//data find
+	for(int i = 0; i < n; i++){
+		
+		std::string find;
+		std::cin >> find;
+		
+		if(data.find(find) != data.end()){
+			count++;
+		}
+	}
+
+	std::cout << count;
 
 	return 0;
 }
