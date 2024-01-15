b0df9d64f6f81a9d3ce6fd4e9b4097733f118aa5
JiHyeonJeong <jh.jeong129@gmail.com>
2024-01-15 17:37:56 +0900

    10816 done!

--- 10816/main.cpp
+++ 10816/main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
+#include <map>
+#include <unordered_map>
 
 int main()
 {
-	
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int n,m;
+
+	std::cin >> n;
+
+	std::unordered_map<int, int> data;
+
+/*
+data input
+
+data struct : {key, count}
+
+count init value is 0, if insert same key value->count increase 1
+*/
+
+
+	for(int i = 0; i < n; i++){
+		
+		int num;
+
+		std::cin >> num;
+
+		if(data.find(num) == data.end()){
+			data.insert({num,1});
+		}
+
+		else{
+			data[num] = data[num]+1;
+		}
+
+	}
+
+	std::cin >> m;
+
+//data find and print count value
+	for(int i = 0; i < m; i++){
+		
+		int num;
+
+		std::cin >> num;
+
+		if(data.find(num) != data.end()){
+			
+			std::cout << data[num] << " ";
+		}
+
+		else{
+			std::cout << "0 ";
+		}
+	}
 
 	return 0;
 }
